f37ca3eef935f13246a2f033561412fc93c09faf
Macarena Sevilla <macarena.sevi@gmail.com>
2020-11-22 18:00:14 +0100

    feat (global.R): update column translator method

--- global.R
+++ global.R
@@ -13,42 +13,16 @@ files = list(open_cases = "https://ias1.larioja.org/opendata/download?r=Y2Q9ODU1
              tests_age_range = "https://ias1.larioja.org/opendata/download?r=Y2Q9ODU4fGNmPTAz", 
              tests_region = "https://ias1.larioja.org/opendata/download?r=Y2Q9ODYxfGNmPTAz")
 
-column_names_translator = list(Region = "ZONA.BASICA",
-                               New_Pos = "NUEVOS.POSITIVOS",
-                               Cum_14_Days = "ACUMULADOS.14.DIAS",
-                               Cum_Inc_14_Days = "IA.14.DIAS", 
-                               CumInc_14Days = "IA14", 
-                               Rate_Inc_14_Days = "TASA.IA.14",
-                               Cum_7_Days = "ACUMULADOS.7.DIAS",
-                               Cum_Inc_7_Days = "IA.7.DIAS", 
-                               CumInc_7Days = "IA7", 
-                               Rate_Inc_7_Days = "TASA.IA.7",
-                               Age_Range = "GRUPO.EDAD",
-                               PCR_Last_14_Days = "PCR.REALIZADOS..ULTIMOS.14.DIAS.",
-                               PCR_Pos_Last_14_Days = "PCR.POSITIVOS..ULTIMOS.14.DIAS.",
-                               PCR_Neg_Last_14_Days = "PCR.NO.POSITIVOS..ULTIMOS.14.DIAS.",
-                               PCR_Cum_Pos = "PCR.POSITIVOS..ACUMULADOS.",
-                               PCR_Cum_Neg = "PCR.NO.POSITIVOS..ACUMULADOS.",
-                               PCR_Cum_Total = "PCR.REALIZADOS..ACUMULADOS.",
-                               Date = "FECHA" ,
-                               Tests_Total = "PRUEBAS.REALIZADAS",
-                               PCR_Total =  "PCR.REALIZADOS" ,
-                               Tests_Pos = "POSITIVOS" ,
-                               Tests_Neg = "NEGATIVOS" ,
-                               Tests_Cum_Pos = "ACUMULADO.POSITIVOS",
-                               Tests_Cum_Neg = "ACUMULADO.NEGATIVOS",
-                               Tests_Cum_Total = "TOTAL.ACUMULADO"  ,
-                               Week = "SEMANA",
-                               Mean_Time_Diagnosis = "TIEMPO.PROMEDIO.DIAGNOSTICO",
-                               Lat = "Lat",
-                               Lon = "Lon",
-                               City = "Title",
-                               New_Detected_Cum_14_Days = "NUEVOS.CASOS.ACUMULADO.14.DIAS",
-                               New_Detected_Cum_7_Days = "NUEVOS.CASOS.ACUMULADO.7.DIAS",
-                               PCR_Test_Cum_Pos = "ACUMULADO.POSITIVO",
-                               PCR_Test_Cum_Neg = "ACUMULADO.NEGATIVO",
-                               City_w_more_than_1000_people = "Localidades.de.más.de.1.000.habitantes",
-                               Active_Cases = "Casos.activos" )
+
+column_names_translator = list(
+  open_cases = c("City_w_more_than_1000_people", "Active_Cases"),
+  mean_time_diagnosis = c("Week", "Mean_Time_Diagnosis"),
+  cumulative_incidence = c("Date", "New_Pos", "Cum_14_Days", "Cum_Inc_14_Days", "Cum_7_Days", "Cum_Inc_7_Days"),
+  cumulative_incidence_region = c("Region", "New_Pos", "Cum_14_Days", "Cum_Inc_14_Days", "Cum_7_Days", "Cum_Inc_7_Days"),
+  tests = c("Date", "Tests_Total", "Tests_Pos", "Tests_Neg", "Tests_Cum_Pos", "Tests_Cum_Neg", "Tests_Cum_Total"),
+  tests_age_range = c("Age_Range", "PCR_Last_14_Days", "PCR_Pos_Last_14_Days", "PCR_Neg_Last_14_Days", "PCR_Cum_Pos", "PCR_Cum_Neg", "PCR_Cum_Total"),
+  tests_region = c("Lat", "Lon", "City", "Tests_Total", "Tests_Cum_Total", "Tests_Pos", "Tests_Neg", "Tests_Cum_Pos", "Tests_Cum_Neg",
+                   "New_Detected_Cum_14_Days", "New_Detected_Cum_7_Days", "Cum_Inc_14_Days", "Cum_Inc_7_Days"))
 
 
 update.files(files)
