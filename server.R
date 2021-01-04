c4b110e6e0e762d9c617606404fb2eb09e76dac0
Macarena Sevilla <macarena.sevi@gmail.com>
2021-01-04 22:55:38 +0100

    feat (server.R): add new plots for testing tab

--- server.R
+++ server.R
@@ -11,20 +11,42 @@ server <- shinyServer(function(input, output) {
   
   output$tests_per_age_range <- renderPlotly({
     tests_age_range_plot = tests_age_range[Age_Range != "DESCONOCIDO"]
-    tests_age_range_plot[, Perc_Positive := PCR_Pos_Last_14_Days*100/(PCR_Last_14_Days)]
-    tests_age_range_plot[, Perc_Negative := PCR_Neg_Last_14_Days*100/(PCR_Last_14_Days)]
+    tests_age_range_plot[, PCR_Total_w_Result := PCR_Pos_Last_14_Days + PCR_Neg_Last_14_Days]
+    tests_age_range_plot[, `Percentage Positive` := round(PCR_Pos_Last_14_Days*100/(PCR_Total_w_Result), 2)]
+    tests_age_range_plot[, `Percentage Negative` := round(PCR_Neg_Last_14_Days*100/(PCR_Total_w_Result), 2)]
+    tests_age_range_plot[, `Total PCR in last 14 days` := PCR_Total_w_Result ]
+    tests_age_range_plot[, `Positive PCR in the last 14 days` :=PCR_Pos_Last_14_Days  ]
     
-    plot <- ggplot(data=tests_age_range_plot, aes(x=Age_Range, y=Perc_Negative, fill='Percentage Negative')) +
-      geom_bar(stat="identity") +
-      geom_bar(aes(x=Age_Range, y=Perc_Positive, fill='Percentage Positive'), stat = "identity")+
+    tests_age_range_plot = melt(tests_age_range_plot, measure.vars = c("Total PCR in last 14 days", "Positive PCR in the last 14 days"))
+    
+    
+    plot <- ggplot(data=tests_age_range_plot, aes(x=Age_Range, y=value, fill=variable, 
+                                                  text = paste0(value), group = 1)) +
+      geom_bar(stat="identity",position = "identity", alpha=.9)+
       scale_fill_brewer(palette = "Pastel2")+
+      scale_x_discrete(labels = tests_age_range$Age_Range)+
       theme_void()+
-      theme(legend.title=element_blank(), legend.position="none")
+      theme(legend.title=element_blank(), legend.position="none", 
+            axis.text.y = element_text(angle=0, hjust=1), 
+            title = element_text(size = 6),
+            plot.title = element_text(size=9),
+            axis.title.x=element_blank(),
+            axis.title.y=element_blank(),
+            legend.key.size=unit(0.4, 'cm'),
+            strip.text.x=element_text(size=7),
+            axis.text=element_text(size=7),
+            axis.text.x=element_text(angle=0, hjust=1),
+            plot.margin=unit(c(0, 0, 0, 0),"pt"))+
+      coord_flip()
     
-    ggplotly(plot) %>% 
+    ggplotly(plot, tooltip = c("text")) %>% 
       layout(plot_bgcolor='transparent', paper_bgcolor='#a6b3bf',
+             hoverlabel=list(bgcolor="white"),
+             hovermode = "x unified",
              font = list(color = '#737373'),
-             margin = list(l = 0, r = 30, b = 0, t = 30, pad = 0))
+             margin = list(l = 0, r = 30, b = 0, t = 30, pad = 0),
+             yaxis = list(gridcolor = "#737373"),
+             xaxis = list(gridcolor = "#737373"))
     
   })
   
@@ -66,6 +88,50 @@ server <- shinyServer(function(input, output) {
     
   })  
   
+  output$test_evolution <- renderPlotly({
+    
+    tests[, Pos_Ratio := round(Tests_Pos*100 / Tests_Total, digits = 2)]
+    
+    plot_cumulative_indidence <- tests %>% 
+      ggplot() + 
+      geom_bar(data = tests, aes(x= Date, y = Tests_Total, color = "Tests_Total",
+                                                text = paste0("Total Tests:", Tests_Total)), stat='identity', color = "lightyellow3") +
+      geom_line(data = tests, aes(x = Date, y = Tests_Pos, color = "Positive Tests",
+                                                 text = paste0("Positive Tests: ", Tests_Pos), group = 1), color = "sienna4") +
+      geom_line(data = tests, aes(x = Date, y = 0, 
+                                                 text = paste0("Date: ", Date), group = 1), color = "darkgrey") +
+      # Custom the Y scales:
+      scale_y_continuous(
+        # Features of the first axis
+        name = "First Axis",
+        # Add a second axis and specify its features
+        sec.axis = sec_axis( trans=~.*10, name="Second Axis")
+      ) +
+      theme(legend.title=element_blank(), legend.position="top",
+            plot.title = element_text(size=9),
+            axis.title.x=element_blank(),
+            axis.title.y=element_blank(),
+            legend.key.size=unit(0.4, 'cm'),
+            strip.text.x=element_text(size=7),
+            axis.text=element_text(size=7),
+            axis.text.x=element_text(angle=0, hjust=1),
+            plot.margin=unit(c(0, 0, 0, 0),"pt")) +
+      scale_x_date(date_breaks = "months" , date_labels = "%Y-%m") +
+      theme(
+        panel.background = element_rect(colour = "white")
+      )
+    
+    #'#f0f2f4'
+    ggplotly(plot_cumulative_indidence, tooltip = "text") %>% 
+      layout(plot_bgcolor='transparent', paper_bgcolor='#a6b3bf',
+             hoverlabel=list(bgcolor="white"),
+             hovermode = "x unified",
+             font = list(color = '#737373'),
+             margin = list(l = 0, r = 30, b = 0, t = 30, pad = 0),
+             yaxis = list(gridcolor = "#737373"),
+             xaxis = list(gridcolor = "#737373"))
+    
+  })  
   
   output$total_open_cases <- renderText(paste({
     sum(open_cases$Active_Cases, na.rm = T)
