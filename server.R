40e56ac7f572bd022eb7c9fc2d6b34e8c7405aa4
Macarena Sevilla <macarena.sevi@gmail.com>
2021-01-07 21:26:22 +0100

    feat (Testing): add plots to study by City and by Weekday

--- server.R
+++ server.R
@@ -18,21 +18,105 @@ server <- shinyServer(function(input, output) {
     tests_age_range_plot[, `Positive PCR in last 14 days` :=PCR_Pos_Last_14_Days  ]
     tests_age_range_plot[, Half_PCR_Pos_Last_14_Days :=PCR_Pos_Last_14_Days  ]
     
-    # tests_age_range_plot = melt(tests_age_range_plot, 
-    #                             measure.vars = c("Total PCR in last 14 days", "Positive PCR in the last 14 days"))
-    
-    
-    plot <- ggplot(data=tests_age_range_plot)+#, aes(x=Age_Range, y=value, fill=variable, 
-                                               #   text = paste0(value), group = 1)) +
+   
+    plot <- ggplot(data=tests_age_range_plot)+
       geom_bar(aes(x=Age_Range, y=`Total PCR in last 14 days`, fill="Total PCR in last 14 days", 
                    text = paste0("Total:", `Total PCR in last 14 days`), group = 1),
                stat="identity",position = "identity", alpha=.9) +
       geom_bar(aes(x=Age_Range, y=`Positive PCR in last 14 days`, fill="Positive PCR in last 14 days", 
                    text = paste0("Positive:", `Positive PCR in last 14 days`, "\nPositive Ratio:", `Percentage Positive`, "%"), 
                    group = 1), stat="identity",position = "identity", alpha=.9) +
-      #geom_bar(stat="identity",position = "identity", alpha=.9)+
       scale_fill_manual(values = c("#fdcdac", "#b3e2cd"))+
       scale_x_discrete(labels = tests_age_range$Age_Range)+
+      theme_void()+
+      theme(legend.title=element_blank(), legend.position="none", 
+            axis.text.y = element_text(angle=0, hjust=1), 
+            title = element_text(size = 6),
+            plot.title = element_text(size=9),
+            axis.title.x=element_blank(),
+            axis.title.y=element_blank(),
+            legend.key.size=unit(0.4, 'cm'),
+            strip.text.x=element_text(size=7),
+            axis.text=element_text(size=7),
+            axis.text.x=element_text(angle=0, hjust=1),
+            plot.margin=unit(c(0, 0, 0, 0),"pt"),
+            text=element_text(angle=90, hjust=1))+
+      coord_flip()
+    
+    ggplotly(plot, tooltip = c("text")) %>% 
+      layout(plot_bgcolor='transparent', paper_bgcolor='#a6b3bf',
+             hoverlabel=list(bgcolor="white"),
+             hovermode = "x unified",
+             font = list(color = '#737373'),
+             margin = list(l = 0, r = 30, b = 0, t = 30, pad = 0),
+             yaxis = list(gridcolor = "#737373"),
+             xaxis = list(gridcolor = "#737373"))
+    
+  })
+  
+  
+  output$tests_region <- renderPlotly({
+    tests_region[ , Cum_Positive_Rate := round(Tests_Cum_Pos*100/Tests_Cum_Total, 2)]
+    tests_region = tests_region[order(-Cum_Positive_Rate)]
+    test_region_plot = tests_region[Tests_Cum_Total >= 200 & Cum_Positive_Rate > 0, ]
+    test_region_plot = test_region_plot[order(-Cum_Positive_Rate)]
+    test_region_plot = test_region_plot[1:15, ]
+    test_region_plot[, City := as.factor(City)]
+    
+    plot <- ggplot(data=test_region_plot, aes(x = City, y= Cum_Positive_Rate))+
+      geom_segment(aes(x=City, 
+                       xend=City, 
+                       y=0, 
+                       yend=Cum_Positive_Rate), 
+                   color = "lightyellow3") + 
+      geom_point(aes(text = paste0(City, "\n Positive Ratio: ", Cum_Positive_Rate, "%")), size=2, color = "sienna4") + 
+      theme_void()+
+      theme(legend.title=element_blank(), legend.position="none", 
+            axis.text.y = element_text(angle=0, hjust=1), 
+            title = element_text(size = 6),
+            plot.title = element_text(size=9),
+            axis.title.x=element_blank(),
+            axis.title.y=element_blank(),
+            legend.key.size=unit(0.4, 'cm'),
+            strip.text.x=element_text(size=7),
+            axis.text=element_text(size=7),
+            axis.text.x=element_text(angle=0, hjust=1),
+            plot.margin=unit(c(0, 0, 0, 0),"pt"),
+            text=element_text(angle=90, hjust=1))+
+      coord_flip()
+    
+    ggplotly(plot, tooltip = c("text")) %>% 
+      layout(plot_bgcolor='transparent', paper_bgcolor='#a6b3bf',
+             hoverlabel=list(bgcolor="white"),
+             hovermode = "x unified",
+             font = list(color = '#737373'),
+             margin = list(l = 0, r = 30, b = 0, t = 30, pad = 0),
+             yaxis = list(gridcolor = "#737373"),
+             xaxis = list(gridcolor = "#737373"))
+    
+  })
+  
+  output$tests_weekday <- renderPlotly({
+    tests_weekday = copy(tests)
+    tests_weekday[, Weekday := c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+                                 "Friday", "Saturday")[wday(tests_weekday$Date)]]
+    tests_weekday[, Tests_Pos_Weekday := sum(Tests_Pos), by=Weekday]
+    tests_weekday[, Tests_Total_Weekday := sum(Tests_Total), by=Weekday]
+    tests_weekday[, Total_Tests_Al_Days := sum(Tests_Total)]
+    tests_weekday[, Weekday_Ratio := round(Tests_Total_Weekday*100/Total_Tests_Al_Days), by=Weekday]
+    tests_weekday[, Pos_Ratio_Weekday := round(Tests_Pos_Weekday*100/Tests_Total_Weekday), by=Weekday]
+    
+    tests_weekday =tests_weekday[!duplicated(tests_weekday, by=c("Weekday", "Tests_Total_Weekday",
+                                                                 "Tests_Pos_Weekday", "Pos_Ratio_Weekday"))]
+    
+    plot <- ggplot(data=tests_weekday)+
+      geom_bar(aes(x=Weekday, y=Tests_Total_Weekday, fill="Tests_Total_Weekday", 
+                   text = paste0("Total Tests:", Tests_Total_Weekday), group = 1),
+               stat="identity",position = "identity", alpha=.9) +
+      geom_bar(aes(x=Weekday, y=Tests_Pos_Weekday, fill="Tests_Pos_Weekday", 
+                   text = paste0("Positive:", Tests_Pos_Weekday, "\nPositive Ratio:", Pos_Ratio_Weekday, "%"), 
+                   group = 1), stat="identity",position = "identity", alpha=.9) +
+      scale_fill_manual(values = c("#fdcdac", "#b3e2cd"))+
       theme_void()+
       theme(legend.title=element_blank(), legend.position="none", 
             axis.text.y = element_text(angle=0, hjust=1), 
